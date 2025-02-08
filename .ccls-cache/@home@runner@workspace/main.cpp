6b4f3ed71c6e54ac7b27b14255b364f493eb7e27
Srivatsan <talkads123@gmail.com>
2025-02-08 04:44:22 +0000

    test Student object in main.cpp

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -3,5 +3,14 @@
 
 int main() {
   Student student1(1);
+  student1.addGrade(90);
+  student1.addGrade(80);
+  student1.addGrade(70);
+  student1.addGrade(60);
+  std::cout << "Student 1 average grade: " << student1.getAverage()
+            << std::endl;
+  std::cout << "Student 1 highest grade: " << student1.getHighestGrade()
+            << std::endl;
+
   return 0;
 }
